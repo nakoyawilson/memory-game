c141da6706d844c1dc596d284f557f6ad26010da
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-04-10 10:22:38 -0400

    Add styles for mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,41 @@
 import "./Header.css";
 import logo from "../assets/logo-dark.svg";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsOpen(true);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <header className="header">
       <h1 className="visually-hidden">Memory Game</h1>
       <img src={logo} className="logo" alt="Memory game logo" />
-      <button className="navigation-btn">Menu</button>
-      <nav>
-        <ul>
+      <button className="navigation-btn" onClick={openMenu}>
+        Menu
+      </button>
+      <nav className={`nav-container ${isOpen && "open"}`}>
+        <ul className="nav-links">
           <li>
-            <button>Restart</button>
+            <button className="nav-btn restart" onClick={closeMenu}>
+              Restart
+            </button>
           </li>
           <li>
-            <button>New Game</button>
+            <button className="nav-btn" onClick={closeMenu}>
+              New Game
+            </button>
+          </li>
+          <li>
+            <button className="nav-btn" onClick={closeMenu}>
+              Resume Game
+            </button>
           </li>
         </ul>
       </nav>
